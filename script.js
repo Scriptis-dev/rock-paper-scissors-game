45c7c47fa6c604bc8c9d6cea20838c380acfd36c
Scriptis-dev <133271683+Scriptis-dev@users.noreply.github.com>
2023-09-10 12:39:08 +0200

    Game Logic Done

---
+++ script.js
@@ -0,0 +1,63 @@
+//Game Logic
+
+let playerScore = 0;
+let computerScore = 0;
+
+const GAME_LOST_MSSG= "Too Bad! Computer won this game!";
+const GAME_WON_MSSG = "Congratulations! You won this game!";
+
+
+function getComputerChoice() {
+  const computerSelection = ["rock", "paper", "scissors"];
+  return computerSelection[
+    Math.floor(Math.random() * computerSelection.length)
+  ];
+}
+
+function getPlayerSelection() {
+  let playerSelectionPrompt = prompt(
+    "Make your choice: ROCK, PAPER or SCISSORS!"
+  ).toLowerCase();
+  return playerSelectionPrompt;
+}
+
+function playRound() {
+  let playerSelection = getPlayerSelection();
+  let computerSelection = getComputerChoice();
+
+  if (playerSelection === computerSelection) {
+    return USER_ROUND_TIE_MSSG;
+  }
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    return USER_ROUND_WON_MSSG;
+  }
+  if (playerSelection === "paper" && computerSelection === "rock") {
+    return USER_ROUND_WON_MSSG;
+  }
+  if (playerSelection === "scissors" && computerSelection === "paper") {
+    return USER_ROUND_WON_MSSG;
+  }
+  return USER_ROUND_LOST_MSSG;
+}
+
+function decideRoundWinner(roundResult) {
+  if(roundResult === USER_ROUND_WON_MSSG) {
+     playerScore += 1;
+  }
+  if (roundResult === USER_ROUND_LOST_MSSG) {
+     computerScore += 1;
+  }
+}
+
+function decideGameWinner() {
+    if (playerScore > computerScore) {
+        alert(GAME_WON_MSSG);
+    }
+    if (playerScore < computerScore) {
+        alert(GAME_LOST_MSSG);
+    }
+    alert('IT\'S A DRAW');
+}
+
+// game();
+// decideGameWinner();
