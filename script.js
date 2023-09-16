0e84bfee0c7c69c175c3f73953853c5404a8d9b2
Scriptis-dev <133271683+Scriptis-dev@users.noreply.github.com>
2023-09-16 11:14:16 +0200

    Score Increment Done

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-//Game Logic
-
-let playerScore = 0;
-let computerScore = 0;
-
-const GAME_LOST_MSSG= "Too Bad! Computer won this game!";
-const GAME_WON_MSSG = "Congratulations! You won this game!";
-
-const USER_ROUND_WON_MSSG = 'You won this round';
-const USER_ROUND_LOST_MSSG = 'You lost this round';
-const USER_ROUND_TIE_MSSG = 'It\s a tie';
-
-function getComputerChoice() {
-  const computerSelection = ["rock", "paper", "scissors"];
-  return computerSelection[
-    Math.floor(Math.random() * computerSelection.length)
-  ];
-}
 
 // function getPlayerSelection() {
 //   let playerSelectionPrompt = prompt(
@@ -23,7 +5,50 @@ function getComputerChoice() {
 //   ).toLowerCase();
 //   return playerSelectionPrompt;
 // }
+//Game Logic
+// //Add to DOM
+// let playerScore = 0;
+// function decideRoundWinner(roundResult) {
+//   if (roundResult === USER_ROUND_WON_MSSG) {
+//     playerScore += 1;
+//   }
+//   if (roundResult === USER_ROUND_LOST_MSSG) {
+//     computerScore += 1;
+//   }
+// }
 
+// function decideGameWinner() {
+//   if (playerScore > computerScore) {
+//     alert(GAME_WON_MSSG);
+//   }
+//   if (playerScore < computerScore) {
+//     alert(GAME_LOST_MSSG);
+//   }
+//   alert("IT'S A DRAW");
+// }
+
+// game();
+// decideGameWinner();
+// let computerScore = 0;
+
+
+
+//---GAME LOGIC
+const GAME_LOST_MSSG = "Too Bad! Computer won this game!";
+const GAME_WON_MSSG = "Congratulations! You won this game!";
+
+const USER_ROUND_WON_MSSG = "You won a round of Rock,Paper & Scissors...now fuck off!";
+const USER_ROUND_LOST_MSSG = "You lost this round...You lost to a fucking computer!";
+const USER_ROUND_TIE_MSSG = "It\'s a fucking draw!";
+
+//Generates random choice as an oponnent
+function getComputerChoice() {
+  const computerSelection = ["rock", "paper", "scissors"];
+  return computerSelection[
+    Math.floor(Math.random() * computerSelection.length)
+  ];
+}
+//Gets players selection 
 function playRound(playerSelection) {
   let computerSelection = getComputerChoice();
   playerSelection = playerSelection.toLowerCase();
@@ -43,52 +68,42 @@ function playRound(playerSelection) {
   return USER_ROUND_LOST_MSSG;
 }
 
-function decideRoundWinner(roundResult) {
-  if(roundResult === USER_ROUND_WON_MSSG) {
-     playerScore += 1;
-  }
-  if (roundResult === USER_ROUND_LOST_MSSG) {
-     computerScore += 1;
-  }
-}
 
-function decideGameWinner() {
-    if (playerScore > computerScore) {
-        alert(GAME_WON_MSSG);
-    }
-    if (playerScore < computerScore) {
-        alert(GAME_LOST_MSSG);
-    }
-    alert('IT\'S A DRAW');
-}
+//---CREATING ELEMENTS
 
-// game();
-// decideGameWinner();
-
-
-
-//Creating Elements
-
-
-const container = document.querySelector('#container');
-const roundScoreDisplay = document.querySelector('#round-score-display')
-
-let buttons = document.querySelectorAll('.btn');
+const container = document.querySelector("#container");
+const roundScoreDisplay = document.querySelector("#round-score-display");
+const playerStats = document.querySelector('#player-stats');
+const computerStats = document.querySelector('#computer-stats');
+let buttons = document.querySelectorAll(".btn");
+let playerRoundResult = 0;
+let computerRoundResult = 0;
 
 buttons.forEach((button) => {
-  button.addEventListener('click', (e, roundScore) => {
-    let playerSelection = e.target.textContent;
-    roundScore = playRound(playerSelection);
-    document.createElement('p').textContent = roundScore;
-    
-    roundScoreDisplay.append(roundScore);
-  });
+  button.addEventListener("click", (e) => {
+    const playerSelection = e.target.textContent;
+    const computerScoreElement = document.getElementById('computer-score');
+    const playerScoreElement = document.getElementById('player-score');
+    const roundScore = playRound(playerSelection);
+
+    //Display the round score
+    const newRoundScoreElement = document.createElement("p");
+    newRoundScoreElement.textContent = roundScore;
+    roundScoreDisplay.append(newRoundScoreElement);
+
+    //Update user score after each round
+      
+    if (roundScore === "You won a round of Rock,Paper & Scissors...now fuck off!") {
+      playerRoundResult++;
+      playerScoreElement.textContent = playerRoundResult;
+      playerStats.append(playerScoreElement);
+    }
+    else if(roundScore === "You lost this round...You lost to a fucking computer!") {
+      computerRoundResult++;
+      computerScoreElement.textContent = computerRoundResult;
+      computerStats.append(computerScoreElement);
+    }
+
+   });
 });
 
-
-function finishGame() {
-} {
-  if (roundScore >= 5) {
-    container.removeChild('#buttons');
-  }
-}
