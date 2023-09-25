84e512330caf239e140f29e5cb0af1d0efab71f1
Scriptis-dev <133271683+Scriptis-dev@users.noreply.github.com>
2023-09-26 00:48:29 +0200

    Game Winner Message Done

--- script.js
+++ script.js
@@ -33,6 +33,8 @@
 
 
 
+
+
 //---GAME LOGIC
 const GAME_LOST_MSSG = "Too Bad! Computer won this game!";
 const GAME_WON_MSSG = "Congratulations! You won this game!";
@@ -48,7 +50,7 @@ function getComputerChoice() {
     Math.floor(Math.random() * computerSelection.length)
   ];
 }
-//Gets players selection 
+//Gets players selection and plays ONE round of the game
 function playRound(playerSelection) {
   let computerSelection = getComputerChoice();
   playerSelection = playerSelection.toLowerCase();
@@ -68,43 +70,61 @@ function playRound(playerSelection) {
   return USER_ROUND_LOST_MSSG;
 }
 
-
 //---CREATING ELEMENTS
-
+// document.addEventListener("click", (e) => console.log(e.target));
 const container = document.querySelector("#container");
 const roundScoreDisplay = document.querySelector("#round-score-display");
-const playerStats = document.querySelector('#player-stats');
-const computerStats = document.querySelector('#computer-stats');
+const playerStats = document.querySelector('.player-stats');
+const computerStats = document.querySelector('.computer-stats');
+const computerScoreElement = document.querySelector('.computer-score');
+const playerScoreElement = document.querySelector('.player-score');
 let buttons = document.querySelectorAll(".btn");
 let playerRoundResult = 0;
 let computerRoundResult = 0;
+let playedRounds = 0;
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const playerSelection = e.target.textContent;
-    const computerScoreElement = document.getElementById('computer-score');
-    const playerScoreElement = document.getElementById('player-score');
     const roundScore = playRound(playerSelection);
-
     //Display the round score
     const newRoundScoreElement = document.createElement("p");
     newRoundScoreElement.textContent = roundScore;
     roundScoreDisplay.append(newRoundScoreElement);
-
-    //Update user score after each round
-      
+    
+    //Update user score after each round      
     if (roundScore === "You won a round of Rock,Paper & Scissors...now fuck off!") {
       playerRoundResult++;
+
       playerScoreElement.textContent = playerRoundResult;
-      playerStats.append(playerScoreElement);
     }
     else if(roundScore === "You lost this round...You lost to a fucking computer!") {
       computerRoundResult++;
+      
       computerScoreElement.textContent = computerRoundResult;
-      computerStats.append(computerScoreElement);
+    }
+    else if(roundScore === "It\'s a fucking draw!") {
+      playerRoundResult++;
+      computerRoundResult++;
+      
+      playerScoreElement.textContent = playerRoundResult;
+      computerScoreElement.textContent = computerRoundResult;
     }
 
-   });
+    playedRounds++;
+
+
+    // console.log(playedRounds);
+    if (playedRounds === 5) {
+      if (playerRoundResult > computerRoundResult) {
+        alert("Congratulations! You won the game!");
+      } else if (playerRoundResult < computerRoundResult) {
+        alert("Sorry, you lost the game.");
+      } else {
+        alert("It's a tie!");
+      }
+    }
+  });
 });
 
 
@@ -186,4 +206,5 @@ buttons.forEach((button) => {
 
 
 
-console.log("dise pakna")
+
+// console.log("dise pakna")
